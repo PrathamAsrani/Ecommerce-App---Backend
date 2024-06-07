19cd126be18cf43a7660257699476da4c902a527
Pratham <114286503+PrathamAsrani@users.noreply.github.com>
2024-06-07 17:32:54 +0530

    Implemented the Login & Register Functionality

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -14,14 +14,14 @@ module.exports.loginController = async (req, res) => {
         const match = await comparePassword(password, user.password);
         if(!match){
             return res.status(200).send({
-                status: false,
+                success: false,
                 message: `Invalid Password`
             });
         }
         // generate token
         const token = await JWT.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
         res.status(200).send({
-            status: true,
+            success: true,
             message: `Logged In successfully`,
             user: {
                 name: user.name,
